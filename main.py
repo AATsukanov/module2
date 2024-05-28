1203f2a918d40bed547cd156713911667d233bab
Alexey A. Tsukanov <a.a.tsukanov@yandex.ru>
2024-05-28 15:13:16 +0300

    добавлено ДЗ-4 и некоторые предыдущие уроки

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    print(u'Привет, PyCharm!')
+    x = 43
+    y = 32
+    print(x * y)
+    print(u'Всё.')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ pep.py
@@ -0,0 +1,3 @@
+import this
+
+print('https://peps.python.org/pep-0008/')
