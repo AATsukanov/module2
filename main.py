0c31014d48da40d5d5aaaba6e9f02e8201873364
Alexey A. Tsukanov <a.a.tsukanov@yandex.ru>
2024-05-30 09:25:19 +0300

    добавлено ДЗ-4 и некоторые предыдущие уроки

--- main.py
+++ main.py
@@ -11,9 +11,20 @@ def main():
     print(x * y)
     print(u'Всё.')
 
+def test(a = 2, b = True):
+    print(f'a = {a}, b = {b}')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
+    #распаковка:
+    d = {'b': False, 'a': '.ru'}
+    print(d)
+    print('\nПодставляем без распаковки:')
+    test(d) #просто встает вместо первого
+    print('\nПодставляем с *:')
+    test(*d) #подставляются только названия ключей
+    print('\nПодставляем с **:')
+    test(**d)  #подставляются значения по ключу!
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
